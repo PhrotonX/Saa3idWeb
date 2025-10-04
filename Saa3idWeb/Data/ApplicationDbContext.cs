2cc27da899b78e627f2ab9f8c5664a480605381c
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-10-04 22:42:10 +0800

    Add DbContext

---
+++ Saa3idWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saa3idWeb.Models;
+
+namespace Saa3idWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		protected ApplicationDbContext()
+		{
+		}
+	    public DbSet<Saa3idWeb.Models.Sample> Sample { get; set; } = default!;
+	}
+}
