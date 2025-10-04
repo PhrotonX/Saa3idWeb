95a3da8de2921c3d1a8eeb31572d504e29c5af6e
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-10-04 22:42:48 +0800

    Add Sample model and its associated Controllers and VIews

---
+++ Saa3idWeb/Models/Sample.cs
@@ -0,0 +1,8 @@
+﻿namespace Saa3idWeb.Models
+{
+	public class Sample
+	{
+		public int Id { get; set; }
+		public String Name { get; set; }
+	}
+}
--- Saa3idWeb/Program.cs
+++ Saa3idWeb/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.EntityFrameworkCore;
+using Saa3idWeb.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+	var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+	options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,5 +37,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// /samples/getall
+app.MapControllerRoute("sample", "{controller=Sample}/{action=GetAll}");
 
 app.Run();
