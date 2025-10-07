d2b3ac4a85fc3dfce31ee8b00482f088701001a1
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-10-07 14:47:25 +0800

    Add migration for locations.LocationType

---
+++ Saa3idWeb/Migrations/20251007064006_mssql_migration_359_add_locationtype_on_locations_table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saa3idWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssql_migration_359_add_locationtype_on_locations_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocationType",
+                table: "Location",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationType",
+                table: "Location");
+        }
+    }
+}
