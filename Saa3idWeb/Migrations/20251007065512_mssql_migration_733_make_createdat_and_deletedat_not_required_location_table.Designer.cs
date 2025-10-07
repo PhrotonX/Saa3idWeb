352ffcf5de3953e4ca2dfbfc40e98c6c465641ba
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-10-07 14:59:44 +0800

    Update Migrations

--- Saa3idWeb/Migrations/20251007065512_mssql_migration_733_make_createdat_and_deletedat_not_required_location_table.Designer.cs
+++ Saa3idWeb/Migrations/20251007065512_mssql_migration_733_make_createdat_and_deletedat_not_required_location_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Saa3idWeb.Data;
 
@@ -11,9 +12,11 @@ using Saa3idWeb.Data;
 namespace Saa3idWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251007065512_mssql_migration_733_make_createdat_and_deletedat_not_required_location_table")]
+    partial class mssql_migration_733_make_createdat_and_deletedat_not_required_location_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
