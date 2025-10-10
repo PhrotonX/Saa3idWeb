fffa3173cbcb2929c12a0f166e009fa4fe172254
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-10-10 20:30:50 +0800

    Update User model

--- Saa3idWeb/Models/User.cs
+++ Saa3idWeb/Models/User.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace Saa3idWeb.Models
 {
-	[Index(nameof(Phone), Name = "Index_Phone_Unique", IsUnique = true)]
-	public class User
+	//[Index(nameof(PhoneNumber), Name = "Index_Phone_Unique", IsUnique = true)]
+	public class User : IdentityUser
 	{
-		public int Id { get; set; }
-
 		[Required(ErrorMessage = "First name is required."), StringLength(255, ErrorMessage = "First name has a minimum of 3 and a maximum of 255 characters.", MinimumLength = 3)]
 		public required string FirstName { get; set; }
 		[StringLength(255, ErrorMessage = "Middle name has a maximum of 255 characters.")]
@@ -16,11 +15,8 @@ namespace Saa3idWeb.Models
 		public required string LastName { get; set; }
 		[StringLength(255, ErrorMessage = "Middle name has a maximum of 255 characters.")]
 		public string? ExtName { get; set; } = null;
-
-		[Required(ErrorMessage = "Phone number is required."), StringLength(255, ErrorMessage = "Phone number has a minimum of 3 and a maximum of 255 characters.", MinimumLength = 3)]
-		public required string Phone { get; set; }
-		[Required(ErrorMessage = "Password is required."), StringLength(255, ErrorMessage = "Password has a minimum of 8 and a maximum of 255 characters.", MinimumLength = 8)]
-		public required string Password { get; set; }
+		[AllowedValues(['F', 'M']), Required(ErrorMessage = "Gender is required")]
+		public char? Gender { get; set; } = null;
 
 		[Required(ErrorMessage = "House/Street/Subdivision is required."), StringLength(255, ErrorMessage = "House/Street/Subdivision has a minimum of 3 and a maximum of 255 characters.", MinimumLength = 3)]
 		public required string HomeAddress { get; set; } //House, Street, Subdivision, etc..
